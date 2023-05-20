3286c2c020c3d8890648b011106750c2722cbae1
Octavio Robleto <octavio.robleto@gmail.com>
2023-05-20 15:53:06 -0400

    se agrega api-docs (swagger)

--- users/src/main/java/com/nisum/exercises/users/controllers/UserController.java
+++ users/src/main/java/com/nisum/exercises/users/controllers/UserController.java
@@ -22,6 +22,12 @@ import com.nisum.exercises.users.services.ResponseErrorMessageService;
 import com.nisum.exercises.users.services.ValidateDataTypeService;
 import com.nisum.exercises.users.utilities.StringUtils;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
@@ -37,6 +43,13 @@ public class UserController implements GenericRestController<User, String> {
 	@Autowired
 	private ValidateDataTypeService dataTypeService;
 
+	@Operation(summary = "Obtiene un Usuario por el id(UUID)")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "OK", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+			@ApiResponse(responseCode = "404", description = "Elemento No Encontrado: ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)) }) })
+
 	public ResponseEntity<?> findById(String id) {
 		logger.debug(id);
 		try {
@@ -44,7 +57,7 @@ public class UserController implements GenericRestController<User, String> {
 
 			User user = userRepository.findById(uuid).orElse(null);
 			if (null == user) {
-				return ResponseEntity.status(200).body(messageService.getErrorMessage(TypeOfMessage.NO_ELEMENTS, id));
+				return ResponseEntity.status(404).body(messageService.getErrorMessage(TypeOfMessage.NO_ELEMENTS, id));
 			}
 
 			return ResponseEntity.ok(user);
@@ -55,15 +68,24 @@ public class UserController implements GenericRestController<User, String> {
 		}
 	}
 
+	@Operation(summary = "Inserta un usuario")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "OK", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+			@ApiResponse(responseCode = "400", description = "Errores de Validacion: ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "409", description = "Usuario ya Registrado: ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)) }) })
+
 	public ResponseEntity<?> insert(@Valid User user, BindingResult bindingResult) {
 		logger.debug(user);
 		ErrorMessage errorMessage = getValidations(user, bindingResult);
 		if (!(null == errorMessage)) {
-			return ResponseEntity.status(200).body(errorMessage);
+			return ResponseEntity.status(400).body(errorMessage);
 		}
 
 		if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-			return ResponseEntity.status(200).body(messageService.getErrorMessage(TypeOfMessage.VALIDATION_ERROR,
+			return ResponseEntity.status(409).body(messageService.getErrorMessage(TypeOfMessage.VALIDATION_ERROR,
 					user.getEmail().concat(" es un correo ya registrado")));
 		}
 
@@ -75,16 +97,25 @@ public class UserController implements GenericRestController<User, String> {
 		}
 	}
 
+	@Operation(summary = "Actualiza un usuario")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "OK", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+			@ApiResponse(responseCode = "400", description = "Errores de Validacion: ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", description = "Elemento No Encontrado: ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)) }) })
+
 	public ResponseEntity<?> update(@Valid User user, BindingResult bindingResult) {
 
 		logger.debug(user);
 		ErrorMessage errorMessage = getValidations(user, bindingResult);
 		if (!(null == errorMessage)) {
-			return ResponseEntity.status(200).body(errorMessage);
+			return ResponseEntity.status(400).body(errorMessage);
 		}
 
 		if (!userRepository.findByEmail(user.getEmail()).isPresent()) {
-			return ResponseEntity.status(200).body(messageService.getErrorMessage(TypeOfMessage.VALIDATION_ERROR,
+			return ResponseEntity.status(404).body(messageService.getErrorMessage(TypeOfMessage.VALIDATION_ERROR,
 					user.getEmail().concat(" No se encuentra registrado")));
 		}
 
@@ -101,9 +132,16 @@ public class UserController implements GenericRestController<User, String> {
 		}
 	}
 
+	@Operation(summary = "Elimina un usuario")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "OK", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+			@ApiResponse(responseCode = "404", description = "Elemento No Encontrado: ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)) }) })
+
 	public ResponseEntity<?> delete(@Valid User user, BindingResult bindingResult) {
 		if (!userRepository.findByEmail(user.getEmail()).isPresent()) {
-			return ResponseEntity.status(200).body(messageService.getErrorMessage(TypeOfMessage.VALIDATION_ERROR,
+			return ResponseEntity.status(404).body(messageService.getErrorMessage(TypeOfMessage.VALIDATION_ERROR,
 					user.getEmail().concat(" No se encuentra registrado")));
 		}
 		try {
@@ -114,6 +152,12 @@ public class UserController implements GenericRestController<User, String> {
 					.body(messageService.getErrorMessage(TypeOfMessage.GENERIC_ERROR, e.getMessage()));
 		}
 	}
+
+	@Operation(summary = "Obtiene un Usuario por el id(UUID)")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "OK", content = {
+			@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = User.class))) }),
+			@ApiResponse(responseCode = "404", description = "Elemento No Encontrado: ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)) }) })
 
 	public ResponseEntity<?> findAll() {
 		List<User> users = userRepository.findAll();
