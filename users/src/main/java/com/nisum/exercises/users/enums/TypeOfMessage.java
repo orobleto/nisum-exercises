27dfca7ed2d4eb5676ead55a9579ec56aedc51e4
Octavio Robleto <octavio.robleto@gmail.com>
2023-05-20 10:18:36 -0400

    Se crea el controlador de los usuarios y el servicio que arma las respuestas de error, se prueba el path (buscar por id), se agrega un tipo de error en el enum

--- users/src/main/java/com/nisum/exercises/users/enums/TypeOfMessage.java
+++ users/src/main/java/com/nisum/exercises/users/enums/TypeOfMessage.java
@@ -1,5 +1,18 @@
 package com.nisum.exercises.users.enums;
 
 public enum TypeOfMessage {
-	VALIDATION_ERROR, GENERIC_ERROR, NO_ELEMENTS, DELETED_ELEMENT;
+	VALIDATION_ERROR("Error de validación de campos: "), GENERIC_ERROR("Error Generico: "),
+	NO_ELEMENTS("No hay elementos que mostrar con: "), DELETED_ELEMENT("Elemento Eliminado: "),
+	DATA_TYPE_ERROR("No es el tipo de dato requerido: ");
+
+	private String message;
+
+	private TypeOfMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
 }
